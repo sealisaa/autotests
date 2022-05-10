ba932558b778f79f0a26f586b92984bd39bd21b8
Алиса <sealisaa@yandex.ru>
2022-05-10 17:47:56 +0300

    added Suite

---
+++ src/test/java/MusicSuiteTest.java
@@ -0,0 +1,7 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ PlayMusicTest.class, AddMusicTest.class, DeleteMusicTest.class })
+public class MusicSuiteTest {
+}
